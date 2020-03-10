eb9aa621c51f8bf527e8cd42ba055e655ce6cbab
Amandacra <amandabolattoancelmo@gmail.com>
2020-03-10 17:51:41 -0300

    exemplos 10/03/2020

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Link para meus exemplos</title>
+</head>
+<body>
+    <h1>Link para meus exemplos:</h1>
+    <p>
+        <h3>Dia 03/03/2020:</h3>
+        <a href="20200303/exemplo_1.html">Exemplo 1</a>
+        <br/>
+        <a href="20200303/exemplo_2.html">Exemplo 2</a>
+        <br/>
+        <a href="20200303/exemplo_3.html">Exemplo 3</a>
+        <br/>
+        <a href="20200303/exemplo_3a.html">Exemplo 3.A</a>
+    </p>
+    <p>
+        <h3>Dia 10/03/2020:</h3>
+        <a href="20200310/exemplo_Onchange.html">Exemplo de Onchange</a>
+        <br/>
+        <a href="20200310/exemplo_Onclick.html">Exemplo de Onclick</a>
+        <br/>
+        <a href="20200310/exemplo_Onblur.html">Exemplo de Onblur</a>
+        <br/>
+        <a href="20200310/exemplo_Onfocus.html">Exemplo de Onfocus</a>
+        <br/>
+        <a href="20200310/exemplo_Onmouseover_Onmouseout.html">Exemplo de Onmouseover e Onmouseout</a>
+        <br/>
+        <a href="20200310/exemplo_Onkeyup_Onkeydown.html">Exemplo de Onkeyup e Onkeydown</a>
+        <br/>
+        <a href="20200310/exemplo_Onload.html">Exemplo de Onload</a>
+    </p>
+</body>
+</html>
